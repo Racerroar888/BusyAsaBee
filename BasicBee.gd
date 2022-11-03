ff3de54d00e931c27803b90f12cd666e9a34e5f4
Racerroar888 <Racerroar888@gmail.com>
2022-11-03 17:40:26 -0600

    Added Swarm Effect
    Works by slightly randomizing the target position compared to the actual mouse position.

--- BasicBee.gd
+++ BasicBee.gd
@@ -4,7 +4,10 @@ extends KinematicBody2D
 export var speed = 100
 
 
+
 func _physics_process(delta):
+	
+	speed = 100
 	
 	if self.position.x > get_global_mouse_position().x:
 		$Sprite.flip_h = false
@@ -12,4 +15,5 @@ func _physics_process(delta):
 	if self.position.x < get_global_mouse_position().x:
 		$Sprite.flip_h = true
 
-	self.position = self.position.move_toward(get_global_mouse_position(), delta * speed)
+	speed = speed * rand_range(0.6,1.4)
+	self.position = self.position.move_toward(Vector2(get_global_mouse_position().x * rand_range(0.8,1.2),get_global_mouse_position().y * rand_range(0.8,1.2)), delta * speed)
